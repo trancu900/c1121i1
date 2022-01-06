17db143126ec2a9fb2cc150d37fdb86d629c1623
trancu900 <pet.cu20791@gmail.com>
2022-01-06 14:54:34 +0700

    fix

---
+++ src/vn/codegym/c1121i1/Main.java
@@ -0,0 +1,7 @@
+package vn.codegym.c1121i1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/vn/codegym/c1121i1/intro/DocSo.java
@@ -0,0 +1,4 @@
+package vn.codegym.c1121i1.intro;
+
+public class DocSo {
+}
